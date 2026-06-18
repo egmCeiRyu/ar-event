2f03af219bfe94600e8e54f8dac3f4a7ac1531a7
egmCeiRyu <egami@ceiryu.com>
2026-06-18 17:19:36 +0900

    Update character.js

--- js/character.js
+++ js/character.js
@@ -1,10 +1,4 @@
-const SUPABASE_URL = "https://btzheezlvxkyemkactvj.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_vOhFbevQUsseGs-oQgm0JQ_8t6Oi1Sh";
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Use o mesmo user_id que aparece na tabela user_stamps
-const USER_ID = "c35d54ae-18c7-4b2f-9338-b0b29...";
+const USER_ID = "c35d54ae-18c7-4b2f-9338-b0b290f9943a";
 
 const characterMap = {
     marker01: 4,
